b9517042e4dd206cb941c1d0adcdea3fee9d96b4
Igor Vedeneev <getmaxx@hotmail.com>
2024-02-19 19:36:22 +0100

    Initial Commit

---
+++ ABNLocationPicker/ABNLocationPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABNLocationPickerApp.swift
+//  ABNLocationPicker
+//
+//  Created by Igor Vedeneev on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ABNLocationPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
